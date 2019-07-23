4d036c394a27486198abbce6c4b4b99c45bdb9f3
Scott DAngelo <scott.dangelo@gmail.com>
2019-07-23 17:14:22 -0600

    Remove run_server.sh info.

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -x
 #export CONVERSATION_IAM_APIKEY=$(echo "$service_watson_conversation" | jq -r '.apikey')
-export CONVERSATION_IAM_APIKEY=y-IeaC53ZFhbnStETOPbtPdq20HXoNGrq8UDLStAp4l3
 #export CONVERSATION_URL=$(echo "$service_watson_conversation" | jq -r '.url')
-export CONVERSATION_URL=https://gateway.watsonplatform.net/assistant/api
 npm start
